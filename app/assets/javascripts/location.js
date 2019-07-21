f968a50f7205391996637d6404efd6e785572ec1
Suman Koirala <sumanrakoirala@gmail.com>
2019-07-21 01:26:39 -0500

    basic lunch thinggy works

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
   function make_form(){
+    console.log("Make Form Function");
     var x = document.getElementById("demo");
-    x.innerHTML = "make Form"
   }
   function success(pos){
+    console.log("Geolocation Success Function");
     var lat = pos.coords.latitude;
     var long = pos.coords.longitude;
     console.log(lat);
@@ -11,35 +12,49 @@ $(document).ready(function() {
 
     if (typeof(lat) != "undefined" && typeof(long) != "undefined")
     {
+      console.log("long and Lat present");
       $.ajax({
         type: "GET",
         url: "/locations_with_long_lat",
         data: "lat="+lat+"&long="+long,
         dataType: "JSON",
         success: function(response){
+          console.log("Success");
           console.log(response);
           var x = document.getElementById("demo");
           x.innerHTML = response['response']['name'];
         },
         fail: function(response){
           console.log("Failed");
+          console.log(response);
         }
       });
     }else{
+      console.log("Long and Lat not present");
       make_form();
     }
   }
-  var pos = navigator.geolocation.getCurrentPosition(success, make_form);
-  // function getLocation() {
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(showPosition);
-  //   } else {
-  //     x.innerHTML = "Geolocation is not supported by this browser.";
-  //   }
-  // }
-  //
-  // function showPosition(position) {
-  //   x.innerHTML = "Latitude: " + position.coords.latitude +
-  //   "<br>Longitude: " + position.coords.longitude;
-  // }
+  //var pos = navigator.geolocation.getCurrentPosition(success, make_form);
+  //console.log("pos");
+  //console.log(pos);
+  get_nearby_place = function (){
+    var place_name = document.getElementById('input_val').value;
+    console.log(place_name);
+    $.ajax({
+      type: "GET",
+      url: "/locations_with_address",
+      data: "place="+place_name,
+      dataType: "JSON",
+      success: function(response){
+        console.log("Success");
+        console.log(response);
+        var x = document.getElementById("demo");
+        x.innerHTML = response['response']['name'];
+      },
+      fail: function(response){
+        console.log("Failed");
+        console.log(response);
+      }
+    });
+  }
 });
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,17 +4,19 @@ class PagesController < ApplicationController
   end
 
   def locations_with_long_lat
-    ap params.permit!
     lat = params[:lat]
     long = params[:long]
     response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{long}&radius=15000&type=restaurant&key=AIzaSyDHwiQbOY5M-bsvr-Dk2g2r1IUVFuDvsao"
-
-    #response = RestClient.get "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurents+fargo&key=AIzaSyDHwiQbOY5M-bsvr-Dk2g2r1IUVFuDvsao"
     val = JSON.parse response
     random = rand(val["results"].length)
     render json: {"response": val["results"][random]}
   end
 
   def locations_with_address
+    place = params[:place]
+    response = RestClient.get "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurents+#{place}&key=AIzaSyDHwiQbOY5M-bsvr-Dk2g2r1IUVFuDvsao"
+    val = JSON.parse response
+    random = rand(val["results"].length)
+    render json: {"response": val["results"][random]}
   end
 end
