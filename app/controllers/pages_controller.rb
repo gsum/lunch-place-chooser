6a70e6dbdb9343cd56494d00495e46ac179ff4a7
Suman Koirala <sumanrakoirala@gmail.com>
2019-07-20 23:11:06 -0500

    google maps work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
+  require 'rest-client'
   def index
     #@lat_lng = cookies[:lat_lng].split("|")
   end
 
   def locations
-    ap "here"
-    render json: {"response": "here"}
+    response = RestClient.get "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurents+fargo&key=AIzaSyDHwiQbOY5M-bsvr-Dk2g2r1IUVFuDvsao"
+    val = JSON.parse response
+    render json: {"response": val["results"]}
   end
 end
